0003eece1e8450f45fdc4b7d75c02a4d87764404
kutta97 <yhjin04170@gmail.com>
2023-02-23 05:04:21 +0900

    2주차: 1436번 - 영화감독 숌

---
+++ week-2/2-M/1436번 - 영화감독 숌.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+    int i = 666;
+    while (true) {
+        if (to_string(i).find("666") != string::npos) n--; 
+        if (n == 0) break;
+        i++;
+    }
+    cout << i << "\n"; 
+
+    return 0;
+}
